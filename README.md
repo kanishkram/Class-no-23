46befbaaac8b64428a61011e74fde0b0eded78f6
kanishkram <71713496+kanishkram@users.noreply.github.com>
2020-11-02 21:19:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class-no-23
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, box;
+var box2 ;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+    box = new Box(200,200,50,50);
+    box2 = new Box(220,100,50,100);
+
+    ground = new Ground(200,390,400,20);
+
+
+  
+
+    
+
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box.display();
+    box2.display();
+  ground.display();
+    
+}
